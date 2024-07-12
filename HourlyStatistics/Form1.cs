1f0b6ce6821b69b3777aa09bd7f677b8a62ca1c7
Alex Li <5796630@mail.ru>
2024-07-12 17:04:50 +0300

    1

--- HourlyStatistics/Form1.cs
+++ HourlyStatistics/Form1.cs
@@ -14,16 +14,16 @@ namespace HourlyStatistics
         {
             int count = 0;
 
-            var hour = dateTime.getHours();
-            hour = ("0" + hour).slice(-2);
-            var endDate = dateTime.toDateString() + "T" + hour + "%3A00%3A00%2B03%3A00";
+            //var hour = dateTime.getHours();
+            //hour = ("0" + hour).slice(-2);
+            //var endDate = dateTime.toDateString() + "T" + hour + "%3A00%3A00%2B03%3A00";
 
-            dateTime.setHours(dateTime.getHours() - 1);
-            hour = dateTime.getHours();
-            hour = ("0" + hour).slice(-2);
-            var strDate = dateTime.toDateString() + "T" + hour + "%3A00%3A00%2B03%3A00";
+            //dateTime.setHours(dateTime.getHours() - 1);
+            //hour = dateTime.getHours();
+            //hour = ("0" + hour).slice(-2);
+            //var strDate = dateTime.toDateString() + "T" + hour + "%3A00%3A00%2B03%3A00";
 
-            string url = $@"http://{IP}/archive/tracks/count?dateStart={strDate}&dateEnd={endDate}";
+            //string url = $@"http://{IP}/archive/tracks/count?dateStart={strDate}&dateEnd={endDate}";
 
 
             return count;
